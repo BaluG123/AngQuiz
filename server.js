4cc9c85cc44da7b92e046275865e7eb01f01669b
balu <balu1072000@gmail.com>
2022-08-13 21:54:32 +0530

    modified package.json file

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist/AngQuiz'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist'}),
-);
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/AngQuiz/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
